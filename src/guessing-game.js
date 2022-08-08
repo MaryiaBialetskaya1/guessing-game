a1946fd0b84b76e11f034ce7fbff50de54fb9f27
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-08-08 18:34:15 -0400

    feat: add solution to task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(assumption, min, max) {
+        this.assumption = assumption;
+        this.min = min;
+        this.max = max;
+    }
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;  
     }
-
     guess() {
-
+        return this.assumption = Math.round((this.min + this.max) / 2)
     }
-
     lower() {
-
+        this.max = this.assumption;
     }
-
     greater() {
-
+        this.min = this.assumption;
     }
 }
 
